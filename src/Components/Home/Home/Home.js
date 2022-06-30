5038b4eccd8c0dec76df9a66c4b744772d12d7b6
Sumonche016 <sumonche016@gmail.com>
2022-07-01 04:00:33 +0600

    last

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -2,6 +2,9 @@ import React, { useEffect, useRef, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useQuery } from 'react-query'
+import Modal from '../Modal/Modal';
+import SingleTask from '../SingleTask/SingleTask';
+import { data } from 'autoprefixer';
 const Home = () => {
 
     const inputRef = useRef()
@@ -43,6 +46,7 @@ const Home = () => {
 
 
         if (inputValue) {
+
             fetch('http://localhost:5000/todo', {
                 method: 'POST',
                 headers: {
@@ -66,15 +70,28 @@ const Home = () => {
     return (
         <div className='bg-[#222222] h-screen'>
             <h1 className='text-[2.5rem] text-white text-center font-semibold'>TODO LIST APP</h1>
-            <form action="" className='w-[40%] mx-auto mt-10' onSubmit={handleSubmit}>
-                <input ref={inputRef} className='outline-0 w-[80%] px-5 py-1.5 text-[#222222] text-left sm:w-72"' type="text" />
-                <button type='submit' className=' text-white px-5 py-1.5 bg-[#1c94d9] duration-150 cursor-pointer hover:bg-[#0081e7]'>Add</button>
-            </form>
 
-            {
-                task?.map(todo => <li className='text-white' key={todo._id}>{todo.inputText} <button onClick={() => handleDelete(todo._id)} class="btn btn-xs">Delete</button></li>)
+            <div className='w-1/2 mx-auto mt-4'>
+                <form action="" className='w-[50%] mx-auto mt-10' onSubmit={handleSubmit}>
+                    <input ref={inputRef} className='outline-0 w-[80%] px-5 py-1.5 text-[#222222] text-left sm:w-72"' type="text" />
+                    <button type='submit' className=' text-white px-5 py-1.5 bg-[#1c94d9] duration-150 cursor-pointer hover:bg-[#0081e7]'>Add</button>
+                </form>
+                <div className='mt-10 flex flex-col items-center'>
+                    {
+                        task?.map(todo => <SingleTask key={todo._id} todo={todo} refetch={refetch}></SingleTask>)
+                    }
+                </div>
+            </div>
+
+
+
+
+
+            {/* {
+                task?.map(todo => <li className='text-white' key={todo._id}>{todo.inputText} <button onClick={() => handleDelete(todo._id)} class="btn btn-xs">Delete</button> <label for="my-modal-6" class="btn modal-button">open modal</label>
+                </li>)
             }
-
+            <Modal></Modal> */}
         </div>
     );
 };
---
+++ src/Components/Home/SingleTask/SingleTask.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const SingleTask = ({ todo, refetch }) => {
+    const { _id, inputText } = todo;
+
+    const handleComplete = (id) => {
+
+        fetch(`http://localhost:5000/todo/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            },
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                refetch()
+            })
+
+
+        const complete = { completeTask: inputText }
+        fetch('http://localhost:5000/complete', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(complete)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+
+
+    }
+    return (
+        <div className='w-[70%] bg-[#e31658] flex items-center justify-between mt-4'>
+            <p className='text-white pl-2'>{inputText}</p>
+            <div>
+                <button onClick={() => handleComplete(_id)} class="px-3 py-2 bg-[#e73422] duration-150 hover:bg-[#b01e15] text-white">✓</button>
+            </div>
+        </div>
+    );
+};
+
+export default SingleTask;
