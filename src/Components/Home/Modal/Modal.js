d3bed149b96d755118faf1621e7320b0ae6d2a37
Sumonche016 <sumonche016@gmail.com>
2022-07-01 05:12:06 +0600

    hiii

--- src/Components/Home/Modal/Modal.js
+++ src/Components/Home/Modal/Modal.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
-const Modal = () => {
+const Modal = (props) => {
+
     return (
         <div>
             <input type="checkbox" id="my-modal-6" class="modal-toggle" />
             <div class="modal modal-bottom sm:modal-middle">
                 <div class="modal-box">
-                    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
-                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                    <div class="modal-action">
-                        <label for="my-modal-6" class="btn">Yay!</label>
-                    </div>
+
+                    <form action="">
+                        <input placeholder={props?.text} className='bg-black text-white' type="text" />
+
+                        <label onClick={handleUpdate} for="my-modal-6" type='submit' class="btn">Yay!</label>
+
+                    </form>
+
+
+
                 </div>
             </div>
         </div>
--- src/Components/Home/SingleTask/SingleTask.js
+++ src/Components/Home/SingleTask/SingleTask.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '../Modal/Modal';
 
 const SingleTask = ({ todo, refetch }) => {
     const { _id, inputText } = todo;
-
+    const [show, setShow] = useState(true)
     const handleComplete = (id) => {
         fetch(`http://localhost:5000/todo/${id}`, {
             method: 'DELETE',
@@ -32,12 +33,21 @@ const SingleTask = ({ todo, refetch }) => {
 
 
     }
+
+
+
+
     return (
         <div className='w-[70%] bg-[#e31658] flex items-center justify-between mt-4'>
             <p className='text-white pl-2'>{inputText}</p>
+            {
+                show && <Modal text={inputText} ></Modal>
+            }
             <div>
+
                 <button onClick={() => handleComplete(_id)} class="px-3 py-2 bg-[#e73422] duration-150 hover:bg-[#b01e15] text-white">✓</button>
             </div>
+
         </div>
     );
 };
